7b3330897886decc37d7d81ce881ea330dfaebec
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2019-01-11 20:47:29 +0100

    add AddressFromPkScript helper function

--- addresshelper/helper.go
+++ addresshelper/helper.go
@@ -2,6 +2,7 @@ package addresshelper
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/decred/dcrd/chaincfg"
 	"github.com/decred/dcrd/dcrutil"
@@ -26,4 +27,18 @@ func DecodeForNetwork(address string, params *chaincfg.Params) (dcrutil.Address,
 		return nil, fmt.Errorf("address %s is not intended for use on %s", address, params.Name)
 	}
 	return addr, nil
+}
+
+func AddressFromPkScript(activeNet *chaincfg.Params, pkScript []byte) (address string, err error) {
+	_, addresses, _, err := txscript.ExtractPkScriptAddrs(txscript.DefaultScriptVersion, pkScript, activeNet)
+	if err != nil {
+		return
+	}
+
+	encodedAddresses := make([]string, len(addresses))
+	for i, address := range addresses {
+		encodedAddresses[i] = address.EncodeAddress()
+	}
+
+	return strings.Join(encodedAddresses, ", "), nil
 }
