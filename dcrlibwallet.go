80fb952ff7937e1e8d0c67f9488739bbeb8d3e86
Ademu Anthony <ademuanthony@gmail.com>
2019-03-06 15:44:17 +0100

    translate wallet errors adequately

--- dcrlibwallet.go
+++ dcrlibwallet.go
@@ -109,6 +109,261 @@ func (lw *LibWallet) Shutdown(exit bool) {
 		} else {
 			log.Infof("Closed wallet")
 		}
+
+		outputs[i] = DecodedOutput{
+			Index:      int32(i),
+			Value:      v.Value,
+			Version:    int32(v.Version),
+			Addresses:  encodedAddrs,
+			ScriptType: scriptClass.String(),
+		}
+	}
+
+	return outputs
+}
+
+func voteVersion(mtx *wire.MsgTx) uint32 {
+	if len(mtx.TxOut[1].PkScript) < 8 {
+		return 0 // Consensus version absent
+	}
+
+	return binary.LittleEndian.Uint32(mtx.TxOut[1].PkScript[4:8])
+}
+
+func voteBits(mtx *wire.MsgTx) uint16 {
+	return binary.LittleEndian.Uint16(mtx.TxOut[1].PkScript[2:4])
+}
+
+func reverse(hash []byte) []byte {
+	for i := 0; i < len(hash)/2; i++ {
+		j := len(hash) - i - 1
+		hash[i], hash[j] = hash[j], hash[i]
+	}
+	return hash
+}
+
+func transactionType(txType wallet.TransactionType) string {
+	switch txType {
+	case wallet.TransactionTypeCoinbase:
+		return "COINBASE"
+	case wallet.TransactionTypeTicketPurchase:
+		return "TICKET_PURCHASE"
+	case wallet.TransactionTypeVote:
+		return "VOTE"
+	case wallet.TransactionTypeRevocation:
+		return "REVOCATION"
+	default:
+		return "REGULAR"
+	}
+}
+
+func (lw *LibWallet) SpendableForAccount(account int32, requiredConfirmations int32) (int64, error) {
+	bals, err := lw.wallet.CalculateAccountBalance(uint32(account), requiredConfirmations)
+	if err != nil {
+		log.Error(err)
+		return 0, err
+	}
+	return int64(bals.Spendable), nil
+}
+
+type txChangeSource struct {
+	version uint16
+	script  []byte
+}
+
+func (src *txChangeSource) Script() ([]byte, uint16, error) {
+	return src.script, src.version, nil
+}
+
+func (src *txChangeSource) ScriptSize() int {
+	return len(src.script)
+}
+
+func makeTxChangeSource(destAddr string) (*txChangeSource, error) {
+	addr, err := dcrutil.DecodeAddress(destAddr)
+	if err != nil {
+		return nil, err
+	}
+	pkScript, err := txscript.PayToAddrScript(addr)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	changeSource := &txChangeSource{
+		script:  pkScript,
+		version: txscript.DefaultScriptVersion,
+	}
+	return changeSource, nil
+}
+
+func (lw *LibWallet) ConstructTransaction(destAddr string, amount int64, srcAccount int32, requiredConfirmations int32, sendAll bool) (*UnsignedTransaction, error) {
+	// output destination
+	addr, err := dcrutil.DecodeAddress(destAddr)
+	if err != nil {
+		return nil, err
+	}
+	pkScript, err := txscript.PayToAddrScript(addr)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	version := txscript.DefaultScriptVersion
+
+	// pay output
+	outputs := make([]*wire.TxOut, 0)
+	var algo wallet.OutputSelectionAlgorithm = wallet.OutputSelectionAlgorithmAll
+	var changeSource txauthor.ChangeSource
+	if !sendAll {
+		algo = wallet.OutputSelectionAlgorithmDefault
+		output := &wire.TxOut{
+			Value:    amount,
+			Version:  version,
+			PkScript: pkScript,
+		}
+		outputs = append(outputs, output)
+	} else {
+		changeSource, err = makeTxChangeSource(destAddr)
+		if err != nil {
+			log.Error(err)
+			return nil, err
+		}
+	}
+	feePerKb := txrules.DefaultRelayFeePerKb
+
+	// create tx
+	tx, err := lw.wallet.NewUnsignedTransaction(outputs, feePerKb, uint32(srcAccount),
+		requiredConfirmations, algo, changeSource)
+	if err != nil {
+		log.Error(err)
+		return nil, translateError(err)
+	}
+
+	if tx.ChangeIndex >= 0 {
+		tx.RandomizeChangePosition()
+	}
+
+	var txBuf bytes.Buffer
+	txBuf.Grow(tx.Tx.SerializeSize())
+	err = tx.Tx.Serialize(&txBuf)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	var totalOutput dcrutil.Amount
+	for _, txOut := range outputs {
+		totalOutput += dcrutil.Amount(txOut.Value)
+	}
+
+	return &UnsignedTransaction{
+		UnsignedTransaction:       txBuf.Bytes(),
+		TotalOutputAmount:         int64(totalOutput),
+		TotalPreviousOutputAmount: int64(tx.TotalInput),
+		EstimatedSignedSize:       tx.EstimatedSignedSerializeSize,
+		ChangeIndex:               tx.ChangeIndex,
+	}, nil
+}
+
+func (lw *LibWallet) SendTransaction(privPass []byte, destAddr string, amount int64, srcAccount int32, requiredConfs int32, sendAll bool) ([]byte, error) {
+	n, err := lw.wallet.NetworkBackend()
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+	defer func() {
+		for i := range privPass {
+			privPass[i] = 0
+		}
+	}()
+	// output destination
+	addr, err := dcrutil.DecodeAddress(destAddr)
+	if err != nil {
+		return nil, err
+	}
+	pkScript, err := txscript.PayToAddrScript(addr)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	// pay output
+	outputs := make([]*wire.TxOut, 0)
+	var algo wallet.OutputSelectionAlgorithm = wallet.OutputSelectionAlgorithmAll
+	var changeSource txauthor.ChangeSource
+	if !sendAll {
+		algo = wallet.OutputSelectionAlgorithmDefault
+		output := &wire.TxOut{
+			Value:    amount,
+			Version:  txscript.DefaultScriptVersion,
+			PkScript: pkScript,
+		}
+		outputs = append(outputs, output)
+	} else {
+		changeSource, err = makeTxChangeSource(destAddr)
+		if err != nil {
+			log.Error(err)
+			return nil, err
+		}
+	}
+
+	// create tx
+	unsignedTx, err := lw.wallet.NewUnsignedTransaction(outputs, txrules.DefaultRelayFeePerKb, uint32(srcAccount),
+		requiredConfs, algo, changeSource)
+	if err != nil {
+		log.Error(err)
+		return nil, translateError(err)
+	}
+
+	if unsignedTx.ChangeIndex >= 0 {
+		unsignedTx.RandomizeChangePosition()
+	}
+
+	var txBuf bytes.Buffer
+	txBuf.Grow(unsignedTx.Tx.SerializeSize())
+	err = unsignedTx.Tx.Serialize(&txBuf)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	var tx wire.MsgTx
+	err = tx.Deserialize(bytes.NewReader(txBuf.Bytes()))
+	if err != nil {
+		log.Error(err)
+		//Bytes do not represent a valid raw transaction
+		return nil, err
+	}
+
+	lock := make(chan time.Time, 1)
+	defer func() {
+		lock <- time.Time{}
+	}()
+
+	err = lw.wallet.Unlock(privPass, lock)
+	if err != nil {
+		log.Error(err)
+		return nil, translateError(err)
+	}
+
+	var additionalPkScripts map[wire.OutPoint][]byte
+
+	invalidSigs, err := lw.wallet.SignTransaction(&tx, txscript.SigHashAll, additionalPkScripts, nil, nil)
+	if err != nil {
+		log.Error(err)
+		return nil, err
+	}
+
+	invalidInputIndexes := make([]uint32, len(invalidSigs))
+	for i, e := range invalidSigs {
+		invalidInputIndexes[i] = e.InputIndex
+	}
+
+	var serializedTransaction bytes.Buffer
+	serializedTransaction.Grow(tx.SerializeSize())
+	err = tx.Serialize(&serializedTransaction)
+	if err != nil {
+		log.Error(err)
+		return nil, err
 	}
 
 	if lw.txIndexDB != nil {
@@ -118,6 +373,12 @@ func (lw *LibWallet) Shutdown(exit bool) {
 		} else {
 			log.Info("tx db closed successfully")
 		}
+		lock <- time.Time{} // send matters, not the value
+	}()
+	err := lw.wallet.Unlock(privPass, lock)
+	if err != nil {
+		log.Error(err)
+		return translateError(err)
 	}
 
 	if exit {
