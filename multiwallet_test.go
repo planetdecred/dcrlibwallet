d7e209196c1348c3d94b81a8952b3951d29a3e4a
Oluwandabira Alawode <dabiraalawode@yahoo.com>
2020-01-22 23:01:51 +0200

    Reexpress the test

--- multiwallet_test.go
+++ multiwallet_test.go
@@ -8,15 +8,30 @@ import (
 	. "github.com/raedahgroup/dcrlibwallet"
 )
 
+const rootDir = ".dcrlibwallet_tests_root"
+
+var _ = BeforeSuite(func() {
+	_, err := os.Stat(rootDir)
+	Expect(os.IsNotExist(err)).To(Equal(true))
+})
+
+// TODO: Add cleanup to delete rootDir if it exists
+
 var _ = Describe("MultiWallet", func() {
-	Context("NewMultiWallet", func() {
-		It("returns a InvalidNetwork error with a wrong netType", func() {
-			dir := ".dcrlibwallet"
-			multi, err := NewMultiWallet(dir, "", "")
-			Expect(multi).To(BeNil())
-			Expect(err).To(Equal(ErrInvalidNetwork))
-			_, err = os.Stat(dir)
-			Expect(os.IsNotExist(err)).To(Equal(true))
+	Describe("NewMultiWallet(rootDir, dbDriver, netType)", func() {
+		Context("when netType is not a valid network", func() {
+			It("shoud fail", func() {
+				multi, err := NewMultiWallet(rootDir, "", "")
+				By("returning a nil wallet")
+				Expect(multi).To(BeNil())
+
+				By("returning an ErrInvalidNetwork")
+				Expect(err).To(Equal(ErrInvalidNetwork))
+
+				By("not creating the directory")
+				_, err = os.Stat(rootDir)
+				Expect(os.IsNotExist(err)).To(Equal(true))
+			})
 		})
 	})
 })
