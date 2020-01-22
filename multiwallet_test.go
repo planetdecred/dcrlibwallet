61cda9e132c020ef5ed245445754ba6fa2708b2f
Oluwandabira Alawode <dabiraalawode@yahoo.com>
2020-01-22 23:55:03 +0200

    Add cleanup

--- multiwallet_test.go
+++ multiwallet_test.go
@@ -12,6 +12,23 @@ const rootDir = ".dcrlibwallet_tests_root"
 
 var _ = Describe("MultiWallet", func() {
 	Describe("NewMultiWallet(rootDir, dbDriver, netType)", func() {
+		AfterEach(func() {
+			err := os.RemoveAll(rootDir)
+			Expect(err).To(BeNil())
+		})
+		Context("when all arguments are valid", func() {
+			It("should return succesfully", func() {
+				multi, err := NewMultiWallet(rootDir, "", "testnet3")
+
+				By("returning a nil error")
+				Expect(err).To(BeNil())
+
+				By("returning a non nil wallet")
+				Expect(multi).ToNot(BeNil())
+
+				multi.Shutdown()
+			})
+		})
 		Context("when netType is not a valid network", func() {
 			It("should fail", func() {
 				_, err := os.Stat(rootDir) // TODO: Extract to BeforeEach
@@ -29,10 +46,13 @@ var _ = Describe("MultiWallet", func() {
 			})
 		})
 		Context(`when dbDriver is not "", "bdb" or "badgerdb"`, func() {
-			It("should return a driver not found", func() {
+			It("should fail", func() {
 				multi, err := NewMultiWallet(rootDir, "nothing", "testnet3")
 
+				By("returning a nil wallet")
 				Expect(multi).To(BeNil())
+
+				By("returning a Driver not found error") //TODO: specify the error
 				Expect(err).ToNot(BeNil())
 			})
 		})
