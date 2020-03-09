d91fe0788d488a64f1374dfc5d988ceba89cd086
ReevesAkwa <reeves.akwa@gmail.com>
2020-03-09 22:10:01 +0100

    documentation for wallets and wallet_config

--- wallet_config.go
+++ wallet_config.go
@@ -5,6 +5,7 @@ import (
 	"github.com/decred/dcrwallet/errors/v2"
 )
 
+//SaveUserConfigValue saves the provided key-value pair to a config database
 func (wallet *Wallet) SaveUserConfigValue(key string, value interface{}) {
 	if wallet.setUserConfigValue == nil {
 		log.Errorf("call wallet.prepare before setting wallet config values")
@@ -17,6 +18,7 @@ func (wallet *Wallet) SaveUserConfigValue(key string, value interface{}) {
 	}
 }
 
+//ReadUserConfigValue returns the saved key-value pair in a config database
 func (wallet *Wallet) ReadUserConfigValue(key string, valueOut interface{}) error {
 	if wallet.setUserConfigValue == nil {
 		log.Errorf("call wallet.prepare before reading wallet config values")
@@ -30,30 +32,44 @@ func (wallet *Wallet) ReadUserConfigValue(key string, valueOut interface{}) erro
 	return err
 }
 
+//SetBoolConfigValueForKey sets a bool value
+// for key-value pair in a config database
 func (wallet *Wallet) SetBoolConfigValueForKey(key string, value bool) {
 	wallet.SaveUserConfigValue(key, value)
 }
 
+//SetDoubleConfigValueForKey sets a float64 value
+// for key-value pair in a config database
 func (wallet *Wallet) SetDoubleConfigValueForKey(key string, value float64) {
 	wallet.SaveUserConfigValue(key, value)
 }
 
+//SetIntConfigValueForKey sets an int value
+// for key-value pair in a config database
 func (wallet *Wallet) SetIntConfigValueForKey(key string, value int) {
 	wallet.SaveUserConfigValue(key, value)
 }
 
+//SetInt32ConfigValueForKey sets an int32 value
+// for key-value pair in a config database
 func (wallet *Wallet) SetInt32ConfigValueForKey(key string, value int32) {
 	wallet.SaveUserConfigValue(key, value)
 }
 
+//SetLongConfigValueForKey sets an int64 value
+// for key-value pair in a config database
 func (wallet *Wallet) SetLongConfigValueForKey(key string, value int64) {
 	wallet.SaveUserConfigValue(key, value)
 }
 
+//SetStringConfigValueForKey sets a string value
+// for key-value pair in a config database
 func (wallet *Wallet) SetStringConfigValueForKey(key, value string) {
 	wallet.SaveUserConfigValue(key, value)
 }
 
+//ReadBoolConfigValueForKey returns the bool value
+// for  key-value pair from a config database
 func (wallet *Wallet) ReadBoolConfigValueForKey(key string, defaultValue bool) (valueOut bool) {
 	if err := wallet.ReadUserConfigValue(key, &valueOut); err == storm.ErrNotFound {
 		valueOut = defaultValue
@@ -61,6 +77,8 @@ func (wallet *Wallet) ReadBoolConfigValueForKey(key string, defaultValue bool) (
 	return
 }
 
+//ReadDoubleConfigValueForKey returns a float64 value
+// for key-value pair from a config database
 func (wallet *Wallet) ReadDoubleConfigValueForKey(key string, defaultValue float64) (valueOut float64) {
 	if err := wallet.ReadUserConfigValue(key, &valueOut); err == storm.ErrNotFound {
 		valueOut = defaultValue
@@ -68,6 +86,8 @@ func (wallet *Wallet) ReadDoubleConfigValueForKey(key string, defaultValue float
 	return
 }
 
+//ReadIntConfigValueForKey returns an int value
+// for a key-value pair from a config database
 func (wallet *Wallet) ReadIntConfigValueForKey(key string, defaultValue int) (valueOut int) {
 	if err := wallet.ReadUserConfigValue(key, &valueOut); err == storm.ErrNotFound {
 		valueOut = defaultValue
@@ -75,6 +95,8 @@ func (wallet *Wallet) ReadIntConfigValueForKey(key string, defaultValue int) (va
 	return
 }
 
+//ReadInt32ConfigValueForKey returns an int32 value
+// for a key-value pair from a config database
 func (wallet *Wallet) ReadInt32ConfigValueForKey(key string, defaultValue int32) (valueOut int32) {
 	if err := wallet.ReadUserConfigValue(key, &valueOut); err == storm.ErrNotFound {
 		valueOut = defaultValue
@@ -82,6 +104,8 @@ func (wallet *Wallet) ReadInt32ConfigValueForKey(key string, defaultValue int32)
 	return
 }
 
+//ReadLongConfigValueForKey returns an int64 value
+// for a key-value pair from a config database
 func (wallet *Wallet) ReadLongConfigValueForKey(key string, defaultValue int64) (valueOut int64) {
 	if err := wallet.ReadUserConfigValue(key, &valueOut); err == storm.ErrNotFound {
 		valueOut = defaultValue
@@ -89,6 +113,8 @@ func (wallet *Wallet) ReadLongConfigValueForKey(key string, defaultValue int64) 
 	return
 }
 
+//ReadStringConfigValueForKey returns the string value
+// for a key-value pair from a config database
 func (wallet *Wallet) ReadStringConfigValueForKey(key string, defaultValue string) (valueOut string) {
 	if err := wallet.ReadUserConfigValue(key, &valueOut); err == storm.ErrNotFound {
 		valueOut = defaultValue
