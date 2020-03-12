f92f1d776d0bdf6283342068518d9e741dbb8c63
Ademu Anthony <ademuanthony@gmail.com>
2020-03-12 16:28:46 +0100

    Go fmt

--- account_test.go
+++ account_test.go
@@ -13,7 +13,7 @@ const rootDir = ".dcrlibwallet_tests_root"
 
 var _ = Describe("Account", func() {
 	var (
-		wallet             *Wallet
+		wallet *Wallet
 	)
 
 	BeforeEach(func() {
@@ -77,7 +77,7 @@ var _ = Describe("Account", func() {
 	Describe("GetAccountBalance(accountNumber int32, requiredConfirmations int32) (*Balance, error)", func() {
 		Context("when called", func() {
 			It("should get the account balance", func() {
-				
+
 				balance, err := wallet.GetAccountBalance(0, 0)
 				By("returning a nil error")
 				Expect(err).To(BeNil())
@@ -90,7 +90,7 @@ var _ = Describe("Account", func() {
 	Describe("SpendableForAccount(account int32, requiredConfirmations int32) (int64, error)", func() {
 		Context("when called", func() {
 			It("should return the spendable balance", func() {
-				
+
 				result, err := wallet.SpendableForAccount(0, 0)
 				By("returning a nil error")
 				Expect(err).To(BeNil())
@@ -103,7 +103,7 @@ var _ = Describe("Account", func() {
 	Describe("NextAccount(accountName string, privPass []byte) (int32, error)", func() {
 		Context("when called", func() {
 			It("should return the next account number", func() {
-				
+
 				accountNumber, err := wallet.NextAccount("account 1", []byte("0000"))
 				By("returning a nil error")
 				Expect(err).To(BeNil())
@@ -111,7 +111,7 @@ var _ = Describe("Account", func() {
 				Expect(accountNumber).To(BeEquivalentTo(int32(1)))
 			})
 		})
-		Context("when called with a wrong password", func () {
+		Context("when called with a wrong password", func() {
 			It("should failed", func() {
 				_, err := wallet.NextAccount("account 1", []byte("1902"))
 				By("returning a non nil error")
@@ -139,7 +139,7 @@ var _ = Describe("Account", func() {
 		})
 		Context("when called with a non-existing account number", func() {
 			It("should fail", func() {
-				
+
 				err := wallet.RenameAccount(56, "first account")
 				By("returning a non nil error")
 				Expect(err).ToNot(BeNil())
@@ -159,7 +159,7 @@ var _ = Describe("Account", func() {
 
 		Context("when called with a non-existing account number", func() {
 			It("should fail", func() {
-				
+
 				name := wallet.AccountName(1220)
 				By("returning 'Account not found'")
 				Expect(name).To(BeEquivalentTo("Account not found"))
@@ -170,7 +170,7 @@ var _ = Describe("Account", func() {
 	Describe("AccountNameRaw(accountNumber uint32) (string, error)", func() {
 		Context("when called with a valid account number", func() {
 			It("should return the account name", func() {
-				
+
 				name, err := wallet.AccountNameRaw(0)
 				By("returning a nil error")
 				Expect(err).To(BeNil())
@@ -180,7 +180,7 @@ var _ = Describe("Account", func() {
 		})
 		Context("when called with a non-existing account number", func() {
 			It("should fail", func() {
-				
+
 				_, err := wallet.AccountNameRaw(10290)
 				By("returning a non nil error")
 				Expect(err).ToNot(BeNil())
@@ -191,7 +191,7 @@ var _ = Describe("Account", func() {
 	Describe("AccountNumber(accountName string) (uint32, error)", func() {
 		Context("when called", func() {
 			It("should return the account number", func() {
-				
+
 				number, err := wallet.AccountNumber("default")
 				By("returning a nil error")
 				Expect(err).To(BeNil())
@@ -204,7 +204,7 @@ var _ = Describe("Account", func() {
 	Describe("HDPathForAccount(accountNumber int32) (string, error)", func() {
 		Context("when called", func() {
 			It("should return the HD path for account", func() {
-				
+
 				var accountNumber int32 = 0
 				path, err := wallet.HDPathForAccount(accountNumber)
 				By("returning a nil error")
