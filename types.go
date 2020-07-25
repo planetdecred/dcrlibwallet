6a84b3554f1cb69a89dc4f4ce45d11720a7c425b
Ademu Anthony <ademuanthony@gmail.com>
2020-07-25 12:24:03 +0100

    VSPd request/response types

--- types.go
+++ types.go
@@ -324,3 +324,46 @@ type UnspentOutput struct {
 	Addresses       string // separated by commas
 	Confirmations   int32
 }
+/** end politea proposal types */
+
+/** begin vspd-related types */
+type GetVspInfoResponse struct {
+	Timestamp     int64   `json:"timestamp"`
+	PubKey        []byte  `json:"pubkey"`
+	FeePercentage float64 `json:"feepercentage"`
+	Closed        bool    `json:"closed"`
+	Network       string  `json:"network"`
+}
+
+type GetFeeAddressRequest struct {
+	Timestamp  int64  `json:"timestamp"`
+	TicketHash string `json:"tickethash"`
+}
+
+type GetFeeAddressResponse struct {
+	TicketHash string `json:"tickethash"`
+	FeeAddress string `json:"feeaddress"`
+	FeeAmount  int64  `json:"feeamount"`
+	Expiration int64  `json:"expiration"`
+}
+
+type PayFeeRequest struct {
+	Timestamp   int64             `json:"timestamp"`
+	TicketHash  string            `json:"tickethash"`
+	FeeTx       string            `json:"feetx"`
+	VotingKey   string            `json:"votingkey"`
+	VoteChoices map[string]string `json:"votechoices"`
+}
+
+type TicketStatusRequest struct {
+	Timestamp  int64  `json:"timestamp"`
+	TicketHash string `json:"tickethash"`
+}
+
+type SetVoteChoicesRequest struct {
+	Timestamp   int64             `json:"timestamp"`
+	TicketHash  string            `json:"tickethash"`
+	VoteChoices map[string]string `json:"votechoices"`
+}
+
+/** end vspd-related types */
