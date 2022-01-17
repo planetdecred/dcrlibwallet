1cdba274e194b8c8090b82026bfbf970e4668133
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2022-01-17 09:30:48 +0100

    add async tx and block notification listeners (#220)
    Prevent potential deadlocks by making provision for tx and
    block notification callbacks to be invoked asynchronously.

--- txandblocknotifications.go
+++ txandblocknotifications.go
@@ -74,7 +74,17 @@ func (mw *MultiWallet) listenForTransactions(walletID int) {
 	}()
 }
 
-func (mw *MultiWallet) AddTxAndBlockNotificationListener(txAndBlockNotificationListener TxAndBlockNotificationListener, uniqueIdentifier string) error {
+// AddTxAndBlockNotificationListener registers a set of functions to be invoked
+// when a transaction or block update is processed by the wallet. If async is
+// true, the provided callback methods will be called from separate goroutines,
+// allowing notification senders to continue their operation without waiting
+// for the listener to complete processing the notification. This asyncrhonous
+// handling is especially important for cases where the wallet process that
+// sends the notification temporarily prevents access to other wallet features
+// until all notification handlers finish processing the notification. If a
+// notification handler were to try to access such features, it would result
+// in a deadlock.
+func (mw *MultiWallet) AddTxAndBlockNotificationListener(txAndBlockNotificationListener TxAndBlockNotificationListener, async bool, uniqueIdentifier string) error {
 	mw.notificationListenersMu.Lock()
 	defer mw.notificationListenersMu.Unlock()
 
@@ -83,7 +93,13 @@ func (mw *MultiWallet) AddTxAndBlockNotificationListener(txAndBlockNotificationL
 		return errors.New(ErrListenerAlreadyExist)
 	}
 
-	mw.txAndBlockNotificationListeners[uniqueIdentifier] = txAndBlockNotificationListener
+	if async {
+		mw.txAndBlockNotificationListeners[uniqueIdentifier] = &asyncTxAndBlockNotificationListener{
+			l: txAndBlockNotificationListener,
+		}
+	} else {
+		mw.txAndBlockNotificationListeners[uniqueIdentifier] = txAndBlockNotificationListener
+	}
 
 	return nil
 }
