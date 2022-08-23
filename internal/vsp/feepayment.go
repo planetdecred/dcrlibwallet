33ea286c19bb7524e33fd43ec477322750d808cc
Wisdom Arerosuoghene <wisdom.arerosuoghene@gmail.com>
2022-08-23 12:37:16 +0100

    test with go 1.17

--- internal/vsp/feepayment.go
+++ internal/vsp/feepayment.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	"decred.org/dcrwallet/v2/errors"
-	"decred.org/dcrwallet/v2/rpc/client/dcrd"
 	"decred.org/dcrwallet/v2/wallet"
 	"decred.org/dcrwallet/v2/wallet/txrules"
 	"decred.org/dcrwallet/v2/wallet/txsizes"
@@ -762,19 +761,7 @@ func (fp *feePayment) reconcilePayment() error {
 			var apiErr *BadRequestError
 			if errors.As(err, &apiErr) && apiErr.Code == codeTicketCannotVote {
 				fp.remove("ticket cannot vote")
-				// Attempt to Revoke Tickets, we're not returning any errors here
-				// and just logging.
-				n, err := w.NetworkBackend()
-				if err != nil {
-					log.Errorf("unable to get network backend for revoking tickets %v", err)
-				} else {
-					if rpc, ok := n.(*dcrd.RPC); ok {
-						err := w.RevokeTickets(ctx, rpc)
-						if err != nil {
-							log.Errorf("cannot revoke vsp tickets %v", err)
-						}
-					}
-				}
+				// Tickets will be automatically revoked.
 			}
 			return err
 		}
